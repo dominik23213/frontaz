ec2d626b81541576e6e91121acd2cd93fb7e1cc6
dominik23213 <dominik23213@gmail.com>
2017-03-08 22:57:59 +0100

    add post in objects arr

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -8,10 +8,23 @@
  * Controller of the frontazApp
  */
 angular.module('frontazApp')
-  .controller('AboutCtrl', function () {
+  .controller('AboutCtrl', function ($scope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
+    ];
+
+    $scope.data = [
+    	{
+    		id: 1,
+    		title: 'post1title',
+    		content: 'eloags asg sas dsa 1'
+    	},
+    	{
+    		id: 2, 
+    		title: 'post2title',
+    		content: 'aggasdgasd gas ad gaeaw sag a 2'
+    	}
     ];
   });
